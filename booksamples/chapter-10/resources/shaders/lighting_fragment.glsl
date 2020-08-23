ae5a73e75388eb61a6748159bc1444c5a279fb6b
ahbejarano <ahbejarano@gmail.com>
2020-08-23 09:49:56 +0200

    Fixed bug in lighting shader

--- booksamples/chapter-10/resources/shaders/lighting_fragment.glsl
+++ booksamples/chapter-10/resources/shaders/lighting_fragment.glsl
@@ -8,5 +8,5 @@ layout(set = 0, binding = 0) uniform sampler2D albedoSampler;
 layout(set = 0, binding = 1) uniform sampler2D depthSampler;
 
 void main() {
-    outFragColor = texture(albedoSampler, inTextCoord).rgb;
+    outFragColor = texture(albedoSampler, inTextCoord);
 }
