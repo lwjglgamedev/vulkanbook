091ee54c5746db5eed07f6694200150c53625b31
James Roome <refuxx@gmail.com>
2023-09-24 16:59:45 +0530

    Run the application with all the needed JVM args

--- booksamples/chapter-05/kotlin/build.gradle.kts
+++ booksamples/chapter-05/kotlin/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.codegen.intrinsics.ArrayOf
+
 plugins {
     kotlin("jvm") version "1.9.0"
     application
@@ -40,4 +42,13 @@ dependencies {
     runtimeOnly("org.lwjgl", "lwjgl-stb", classifier = lwjglNatives)
     runtimeOnly("org.lwjgl", "lwjgl-vulkan", classifier = lwjglNatives)
     implementation("org.joml", "joml", jomlVersion)
+}
+
+application {
+    applicationDefaultJvmArgs = listOf(
+        "-XstartOnFirstThread",
+        "-Dorg.slf4j.simpleLogger.defaultLogLevel=DEBUG",
+        "-Dorg.lwjgl.librarypath=/usr/local/lib",
+        "-Dorg.lwjgl.vulkan.libname=libvulkan.1.dylib")
+    mainClass = "org.vulkanb.Main"
 }
